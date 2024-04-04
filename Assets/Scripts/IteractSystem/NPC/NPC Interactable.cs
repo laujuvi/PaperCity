056a6302fef503e8416c1bdc25c3e2f0313804d1
Mateotorija <mateotorija@gmail.com>
2024-04-04 15:30:45 -0300

    sistema de interaccion con NPC

--- Assets/Scripts/IteractSystem/NPC/NPC Interactable.cs
+++ Assets/Scripts/IteractSystem/NPC/NPC Interactable.cs
@@ -2,10 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPCInteractable : MonoBehaviour
+public class NPCInteractable : MonoBehaviour, IInteractable
 {
+    [SerializeField] private string interactText;
     public void Interact()
     {
+        Debug.Log("INTERACTUASTE");
+    }
 
+    public string GetInteractText()
+    {
+        return interactText;
+    }
+
+    public Transform GetTransform()
+    {
+        return transform;
     }
 }
--- Assets/Scripts/IteractSystem/Player/PlayerInteract.cs
+++ Assets/Scripts/IteractSystem/Player/PlayerInteract.cs
@@ -11,14 +11,49 @@ public class PlayerInteract : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.E))
         {
-            Collider[] colliderArray = Physics.OverlapSphere(transform.position, _InteractRange);
-            foreach(Collider collider in colliderArray)
+            IInteractable interactable = GetInteractableObject();
+            if (interactable != null)
             {
-                if (collider.TryGetComponent(out NPCInteractable npcInteractable))
+                interactable.Interact();
+            }          
+        }
+    }
+
+    public IInteractable GetInteractableObject()
+    {
+        List<IInteractable> InteractableList = new List<IInteractable>();
+
+        Collider[] colliderArray = Physics.OverlapSphere(transform.position, _InteractRange);
+        foreach (Collider collider in colliderArray)
+        {
+            if (collider.TryGetComponent(out IInteractable interactable))
+            {
+                InteractableList.Add(interactable); 
+            }
+        }
+
+        IInteractable closestInteractable = null;
+        foreach(IInteractable interactable in InteractableList)
+        {
+            if(closestInteractable == null)
+            {
+                closestInteractable = interactable;
+            }
+            else
+            {
+                if (Vector3.Distance(transform.position, interactable.GetTransform().position) <
+                    Vector3.Distance(transform.position, closestInteractable.GetTransform().position))
                 {
-                    npcInteractable.Interact();
+                    closestInteractable = interactable;
                 }
             }
         }
+        return closestInteractable;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _InteractRange);
     }
 }
