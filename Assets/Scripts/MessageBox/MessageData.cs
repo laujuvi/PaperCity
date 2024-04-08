266cd8179568822d008049a90e34b9d6b1b740ac
laujuvi <laujuvi@hotmail.com>
2024-04-07 21:52:02 -0300

    Creo MessageData para gestionar el queue

---
+++ Assets/Scripts/MessageBox/MessageData.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public enum Emotions { Neutral, Angry, Talking, Thinking };
+
+public class MessageData
+{
+    public string Name { get; }
+    public Color Color { get; }
+    public string Message { get; }
+    public Emotions Emotion { get; }
+
+    public MessageData(string name, Color color, string message, Emotions emotion)
+    {
+        Name = name;
+        Color = color;
+        Message = message;
+        Emotion = emotion;
+    }
+}
