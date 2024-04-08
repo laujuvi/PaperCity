de5a69d404602b4203a38912e9153e647b71f9ba
laujuvi <laujuvi@hotmail.com>
2024-04-07 21:53:22 -0300

    Update NPC Interactable.cs

--- Assets/Scripts/IteractSystem/NPC/NPC Interactable.cs
+++ Assets/Scripts/IteractSystem/NPC/NPC Interactable.cs
@@ -4,9 +4,30 @@ using UnityEngine;
 
 public class NPCInteractable : MonoBehaviour, IInteractable
 {
+
     [SerializeField] private string interactText;
+    private BoxMessageManager boxMessageManager;
+
+    private bool isDialog2 = false;
+    private bool isDialog3 = false;
+    private bool isDialog4 = false;
+    private int interects = 0;
+
+    public void Start()
+    {
+        boxMessageManager = FindObjectOfType<BoxMessageManager>();
+
+        if (boxMessageManager == null)
+        {
+            Debug.LogError("No se encontró un BoxMessageManager en la escena.");
+            return;
+        }
+    }
+
+
     public void Interact()
     {
+        TestMessages();
         Debug.Log("INTERACTUASTE");
     }
 
@@ -18,5 +39,31 @@ public class NPCInteractable : MonoBehaviour, IInteractable
     public Transform GetTransform()
     {
         return transform;
+    }
+
+    // ESTE METODO ES A MODO PRUEBA
+    private void TestMessages()
+    {
+        if (interects <= 1)
+        {
+            boxMessageManager.SendMessage("NPC", Color.blue, "Hello, player!", Emotions.Neutral);
+            interects++;
+            
+        }
+        else if (!isDialog2)
+        {
+            boxMessageManager.SendMessage("NPC", Color.blue, "Why do you keep bothering me?", Emotions.Neutral);
+            isDialog2 = true;
+        }
+        else if (!isDialog3)
+        {
+            boxMessageManager.SendMessage("NPC", Color.blue, "I'm getting really annoyed now.", Emotions.Angry);
+            isDialog3 = true;
+        }
+        else if (!isDialog4)
+        {
+            boxMessageManager.SendMessage("NPC", Color.blue, "That's it, I'm done talking to you.", Emotions.Angry);
+            isDialog4 = true;
+        }
     }
 }
