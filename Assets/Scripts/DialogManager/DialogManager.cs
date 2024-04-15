68cad3253a84687c708d17f5657eb7aa8417fa6c
laujuvi <laujuvi@hotmail.com>
2024-04-15 20:34:00 -0300

    Update DialogManager.cs

--- Assets/Scripts/DialogManager/DialogManager.cs
+++ Assets/Scripts/DialogManager/DialogManager.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class EvidenceData
 {
     public bool hasEvidence;
-    public string evidenseName;
+    public string evidenceName;
+    public string requiredMessage;
 }
 
 [System.Serializable]
@@ -14,7 +15,7 @@ public class DialogMessage
     public bool talked;
     public EvidenceData evidence;
     public string message;
-    public Emotions emotion;
+    public string emotion;
 }
 
 [System.Serializable]
@@ -35,9 +36,12 @@ public class DialogManager : MonoBehaviour
 {
     [SerializeField] private TextAsset dialoguesJson;
     [SerializeField] private GameObject[] evidenceArray;
+    private Dictionary<string, bool> evidenceStatus = new Dictionary<string, bool>();
+
     private BoxMessageManager boxMessageManager;
 
     private DialoguesWrapper dialoguesWrapper;
+
 
     private void Start()
     {
@@ -49,6 +53,12 @@ public class DialogManager : MonoBehaviour
         else
         {
             Debug.LogError("Dialogues JSON not assigned.");
+        }
+
+        // Agregar los objetos al diccionario evidenceStatus
+        foreach (GameObject evidenceObject in evidenceArray)
+        {
+            evidenceStatus.Add(evidenceObject.name, false);
         }
     }
 
@@ -69,7 +79,7 @@ public class DialogManager : MonoBehaviour
                 {
                     if (!message.evidence.hasEvidence)
                     {
-                        boxMessageManager.SendMessage(dialog.name, dialog.color, message.message, message.emotion);
+                        boxMessageManager.SendMessage(dialog.name, dialog.color, message.message, (Emotions)System.Enum.Parse(typeof(Emotions), message.emotion));
                         message.talked = true;
                         return;
                     }
@@ -77,23 +87,27 @@ public class DialogManager : MonoBehaviour
                     {
                         foreach (GameObject evidenceObject in evidenceArray)
                         {
-                            if (evidenceObject.name == message.evidence.evidenseName)
+                            if (evidenceObject.name == message.evidence.evidenceName && GetEvidenceStatus(evidenceObject.name) )
                             {
-                                // El objeto existe en evidenceArray
-                                Debug.Log("Requiere evidencia: " + message.evidence.evidenseName);
+                                boxMessageManager.SendMessage(dialog.name, dialog.color, message.message, (Emotions)System.Enum.Parse(typeof(Emotions), message.emotion));
+                                message.talked = true;
+                                return;
+                            } else
+                            {
+                                boxMessageManager.SendMessage(dialog.name, dialog.color, message.evidence.requiredMessage, (Emotions)System.Enum.Parse(typeof(Emotions), message.emotion));
                                 return;
                             }
                         }
 
                         // El objeto no existe en evidenceArray
-                        Debug.Log("No se encontró la evidencia: " + message.evidence.evidenseName);
+                        Debug.Log("No se encontró la evidencia: " + message.evidence.evidenceName);
                     }
                 }
 
             }
         }
 
-        Debug.LogError("Dialog not found or all messages require evidence.");
+        Debug.LogWarning("Dialog not found or all messages require evidence.");
     }
 
     private DialogData FindDialogByName(string name)
@@ -107,5 +121,30 @@ public class DialogManager : MonoBehaviour
         }
 
         return null;
+    }
+
+    public void SetEvidenceStatus(string evidenceName, bool status)
+    {
+        if (evidenceStatus.ContainsKey(evidenceName))
+        {
+            evidenceStatus[evidenceName] = status;
+        }
+        else
+        {
+            Debug.LogError("Evidence with name " + evidenceName + " not found in evidenceArray.");
+        }
+    }
+
+    public bool GetEvidenceStatus(string evidenceName)
+    {
+        if (evidenceStatus.ContainsKey(evidenceName))
+        {
+            return evidenceStatus[evidenceName];
+        }
+        else
+        {
+            Debug.LogError("Evidence with name " + evidenceName + " not found in evidenceArray.");
+            return false;
+        }
     }
 }
