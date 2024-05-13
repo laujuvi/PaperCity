68156742262c9c492b39664b0b30263f75d448a4
laujuvi <laujuvi@hotmail.com>
2024-05-12 23:42:53 -0300

    Modifico GameManager para condicion win-lose

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    /* MANAGERS */
+    [SerializeField] private BoxMessageManager boxMessageManager;
+    [SerializeField] private DialogManager dialogManager;
+    
+    /* DELAY */
     [SerializeField] private float _delay;
     public PlayerInteract _playerInteract;
     public PlayerController _playerController;
+
+    /* EVIDENCE MONITORING */
+    [SerializeField] private int minEvidence = 1;
+    [SerializeField] private int maxEvidence = 2;
+    [SerializeField] private GameObject guiltyNPC;
+    [SerializeField] private string intermediateMessage;
+    [SerializeField] private string definitiveMessage;
+    private int currentEvidence = 0;
+
     private void Start()
     {
         if (_playerInteract != null)
@@ -29,5 +43,38 @@ public class GameManager : MonoBehaviour
         yield return new WaitForSeconds(delay);
         _playerInteract.enabled = true;
         _playerController.enabled = true;
+    }
+
+    public void CheckGuiltyNPC(string NPCName)
+    {
+        if (currentEvidence < minEvidence) return;
+        while (!boxMessageManager.IsDisplayingMessage())
+        {
+            if (guiltyNPC.name == NPCName)
+            {
+                Debug.Log("WIN");
+                return;
+            } else
+            {
+                Debug.Log("LOSE");
+                return;
+            }
+        }
+
+    }
+
+    public void CheckCurrentEvidence()
+    {
+        currentEvidence++;
+        if (currentEvidence >= maxEvidence) { 
+        boxMessageManager.SendMessage("", Color.white, definitiveMessage, Emotions.None);
+            return;
+        }
+
+        if (currentEvidence >= minEvidence)
+        {
+            boxMessageManager.SendMessage("", Color.white, intermediateMessage, Emotions.None);
+            return;
+        }
     }
 }
