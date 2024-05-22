6b94fb072e11f98f15ab3df26aeddddde620cb0f
AaronHChoi <108954995+AaronHChoi@users.noreply.github.com>
2024-05-22 20:08:58 -0300

    agrego textura al piso modificacion de codigo del loot at player

--- Assets/Scripts/Npc/LookToPlayer.cs
+++ Assets/Scripts/Npc/LookToPlayer.cs
@@ -7,6 +7,8 @@ public class LookToPlayer : MonoBehaviour
     [SerializeField] private GameObject _objective;
     [SerializeField] private float _range;
 
+    Vector3 _direction;
+
     void Update()
     {
         LookPlayer();
@@ -17,7 +19,13 @@ public class LookToPlayer : MonoBehaviour
         if (Vector3.Distance(transform.position, _objective.transform.position) <= _range)
         {
             //transform.rotation = Quaternion.RotateTowards(transform.rotation, _objective.transform.rotation, 0.5f);
-            transform.LookAt(_objective.transform);
+            //transform.LookAt(_objective.transform);
+
+            _direction = _objective.transform.position - transform.position;
+            _direction.y = 0;
+            
+            transform.rotation = Quaternion.LookRotation(_direction);
+
         }  
     }
 
