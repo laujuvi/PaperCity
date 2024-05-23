0cb8b199a5f4f1f2f5c83991a976baf36a5bc9db
francorivaas <65259655+francorivaas@users.noreply.github.com>
2024-05-23 15:42:30 -0300

    new dialogue box added

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -48,51 +48,63 @@ public class PlayerController : MonoBehaviour
     }
     private void Move()
     {
-        if(characterController.isGrounded)
+        if (!PlayerDialogue.isHavingDialogue)
         {
-            moveInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-            moveInput = Vector3.ClampMagnitude(moveInput, 1f);
+            if(characterController.isGrounded)
+            {
+                moveInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+                moveInput = Vector3.ClampMagnitude(moveInput, 1f);
 
-            if (Input.GetButton("Sprint"))
-            {
-                moveInput = transform.TransformDirection(moveInput) * runSpeed;
+                if (Input.GetButton("Sprint"))
+                {
+                    moveInput = transform.TransformDirection(moveInput) * runSpeed;
+                }
+                else
+                {
+                    moveInput = transform.TransformDirection(moveInput) * walkSpeed;
+                }
+                if (Input.GetButtonDown("Jump"))
+                {
+                    moveInput.y = Mathf.Sqrt(jumpHeight * -2f * gravityScale);
+                }
             }
-            else
-            {
-                moveInput = transform.TransformDirection(moveInput) * walkSpeed;
-            }
-            if (Input.GetButtonDown("Jump"))
-            {
-                moveInput.y = Mathf.Sqrt(jumpHeight * -2f * gravityScale);
-            }
+            moveInput.y += gravityScale * Time.deltaTime;
+            characterController.Move(moveInput * Time.deltaTime);
         }
-        moveInput.y += gravityScale * Time.deltaTime;
-        characterController.Move(moveInput * Time.deltaTime);
     }
     private void Look()
     {
-        rotationInput.x = Input.GetAxis("Mouse X") * rotationSensibility * Time.deltaTime;
-        rotationInput.y = Input.GetAxis("Mouse Y") * rotationSensibility * Time.deltaTime;
+        if (PlayerDialogue.isHavingDialogue)
+        {
+            rotationInput.x = Input.GetAxis("Mouse X") * rotationSensibility * Time.deltaTime;
+            rotationInput.y = Input.GetAxis("Mouse Y") * rotationSensibility * Time.deltaTime;
 
-        cameraVerticalAngle += rotationInput.y;
-        cameraVerticalAngle = Mathf.Clamp(cameraVerticalAngle, -70, 70);
+            cameraVerticalAngle += rotationInput.y;
+            cameraVerticalAngle = Mathf.Clamp(cameraVerticalAngle, -70, 70);
 
-        transform.Rotate(Vector3.up * rotationInput.x);
-        playerCamera.transform.localRotation = Quaternion.Euler(-cameraVerticalAngle, 0f, 0f);
+            transform.Rotate(Vector3.up * rotationInput.x);
+            playerCamera.transform.localRotation = Quaternion.Euler(-cameraVerticalAngle, 0f, 0f);
+        }
     }
     private void Crouch()
     {
-        crouch = Input.GetKey(KeyCode.LeftControl);
+        if (!PlayerDialogue.isHavingDialogue)
+        {
+            crouch = Input.GetKey(KeyCode.LeftControl);
 
-        float crouchLocalScaleY = crouch ? crouchHeight : 1f;
-        float newCrouchScaleY = Mathf.Lerp(transform.localScale.y, crouchLocalScaleY, Time.deltaTime * smooth);
-        transform.localScale = new Vector3(1, newCrouchScaleY, 1);
+            float crouchLocalScaleY = crouch ? crouchHeight : 1f;
+            float newCrouchScaleY = Mathf.Lerp(transform.localScale.y, crouchLocalScaleY, Time.deltaTime * smooth);
+            transform.localScale = new Vector3(1, newCrouchScaleY, 1);
+        }
     }
     private void LookUp()
     {
-        lookUp = Input.GetKey(KeyCode.V);
-        float targetLocalScaleY = lookUp ? lookUphHeight : 1f;
-        float newScaleY = Mathf.Lerp(transform.localScale.y, targetLocalScaleY, Time.deltaTime * smooth);
-        transform.localScale = new Vector3(1, newScaleY, 1);
+        if (!PlayerDialogue.isHavingDialogue)
+        {
+            lookUp = Input.GetKey(KeyCode.V);
+            float targetLocalScaleY = lookUp ? lookUphHeight : 1f;
+            float newScaleY = Mathf.Lerp(transform.localScale.y, targetLocalScaleY, Time.deltaTime * smooth);
+            transform.localScale = new Vector3(1, newScaleY, 1);
+        }
     }
 }
