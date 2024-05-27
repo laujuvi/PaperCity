df7a02c5d316a25eb237230ff2939ce2135a6ea7
julianKim11 <juliankim0911@gmail.com>
2024-05-27 13:14:27 -0300

    Marcos de las puertas

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -10,12 +10,6 @@ public class UIManager : MonoBehaviour
     private int currentEvidence = 0;
     private int totalEvidence = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
     public void UpdateCurrentEvidence(int currentInt)
     {
         currentEvidence = currentInt;
