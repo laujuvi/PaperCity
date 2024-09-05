79994b745d191ec78f25df6451d8bf0c264f0d1b
Mateotorija <mateotorija@gmail.com>
2024-09-05 11:21:54 -0300

    prueba

---
+++ Assets/Scripts/IteractSystem/Clue/BasicClueInteract.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicClueInteract : MonoBehaviour, IInteractable
+{
+    [SerializeField] private string interactText;
+    [SerializeField] public string pickableText;
+    [SerializeField] public string pickablePJText = "";
+
+    private BoxMessageManager boxClueMessageManager;
+
+
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxClueMessageManager = FindObjectOfType<BoxMessageManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public virtual void Interact()
+    {
+        if (gameObject.layer == LayerMask.NameToLayer("Pickeable"))
+        {
+           boxClueMessageManager.SendMessage(pickablePJText, Color.white, pickableText, Emotions.None);
+           Destroy(gameObject);
+        }
+   
+    }
+    public string GetInteractText()
+    {
+        return interactText;
+    }
+
+    public Transform GetTransform()
+    {
+        return transform;
+    }
+
+
+}
---
+++ Assets/Scripts/IteractSystem/Clue/ClueInteract.cs
@@ -0,0 +1,47 @@
+using System.Buffers.Text;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ClueInteract : BasicClueInteract
+{
+    [SerializeField] private string description;
+    private DialogManager dialogManager;
+    private GameManager gameManager;
+
+    [Header("ListManager")]
+    private ListManager _listManager;
+
+    [Header("Audio Source")]
+    [SerializeField] private AudioManager audioManager;
+
+    private BoxMessageManager boxClueMessageManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogManager = FindObjectOfType<DialogManager>();
+        gameManager = FindObjectOfType<GameManager>();
+        _listManager = FindObjectOfType<ListManager>();
+        boxClueMessageManager = FindObjectOfType<BoxMessageManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void Interact()
+    {
+        boxClueMessageManager.SendMessage(base.pickablePJText, Color.white, base.pickableText, Emotions.None);
+        dialogManager.SetEvidenceStatus(gameObject.name, true);
+        _listManager.AddText($"({description})");
+        gameManager.CheckCurrentEvidence();
+        //audioManager.PlaySFX(audioManager.clueFound);
+        print("pick");
+        //gameObject.SetActive(false);
+        Destroy(gameObject);
+        
+    }
+}
