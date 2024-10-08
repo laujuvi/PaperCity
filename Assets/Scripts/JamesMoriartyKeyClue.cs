c1f490b037f74d57cfd77cff41315381b9ad2094
francorivaas <francoagustinrivas@hotmail.com>
2024-10-08 17:04:15 -0300

    fixes

--- Assets/Scripts/JamesMoriartyKeyClue.cs
+++ Assets/Scripts/JamesMoriartyKeyClue.cs
@@ -6,10 +6,12 @@ public class JamesMoriartyKeyClue : MonoBehaviour
 {
     public GameObjectControllerv2 goc2;
     public GameObject jamesMoriartyKey;
+   
     private void Start()
     {
         goc2.AddObject("JamesMoriartyKey", jamesMoriartyKey);
     }
+    
     public void CheckCountDialog(int countDialog)
     {
         if (countDialog >= 3)
