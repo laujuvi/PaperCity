0c9874693b56b1c7030e190ec2facaa09687f2dc
laujuvi <laujuvi@hotmail.com>
2024-10-22 20:02:54 -0300

    Update GuiltyRoom.cs
    Desactivo puertas desde el guiltyRoom

--- Assets/Scripts/GuiltyRoom/GuiltyRoom.cs
+++ Assets/Scripts/GuiltyRoom/GuiltyRoom.cs
@@ -17,6 +17,7 @@ public class GuiltyRoom : MonoBehaviour, IInteractable
         if (GameManager.Instance.currentEvidence >= guiltyRoomManager.minClue)
         {
             GameManager.Instance.isPlayerInGuiltyRoom = true;
+            GameManager.Instance.enableDisableOutlineDoors(false);
             guiltyRoomManager.GuiltyRoomUIPanel.SetActive(true);
             GameManager.Instance.ShowCursor();
             guiltyRoomManager.PauseGame();
