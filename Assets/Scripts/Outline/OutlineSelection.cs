7000fb0b464912f466b55057b2b5c86cfad542ad
AaronHChoi <108954995+AaronHChoi@users.noreply.github.com>
2024-10-31 19:57:27 -0300

    limpiado del script

--- Assets/Scripts/Outline/OutlineSelection.cs
+++ Assets/Scripts/Outline/OutlineSelection.cs
@@ -9,86 +9,28 @@ public class OutlineSelection : MonoBehaviour
     private Transform selection;
     private RaycastHit raycastHit;
 
-    public float interactDistance = 2f;
+    public float interactDistance = 4f;
 
     public Transform player;
 
     void Update()
     {
-        //// Highlight
-        //if (highlight != null)
-        //{
-        //    highlight.gameObject.GetComponent<Outline>().enabled = false;
-        //    highlight = null;
-        //}
-        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        //if (!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out raycastHit)) //Make sure you have EventSystem in the hierarchy before using EventSystem
-        //{
-        //    highlight = raycastHit.transform;
-        //    if (highlight.CompareTag("Selectable") && highlight != selection)
-        //    {
-        //        if (highlight.gameObject.GetComponent<Outline>() != null)
-        //        {
-        //            highlight.gameObject.GetComponent<Outline>().enabled = true;
-
-        //        }
-        //        else
-        //        {
-        //            Outline outline = highlight.gameObject.AddComponent<Outline>();
-        //            outline.enabled = true;
-        //            highlight.gameObject.GetComponent<Outline>().OutlineColor = Color.magenta;
-        //            highlight.gameObject.GetComponent<Outline>().OutlineWidth = 10.0f;
-        //        }
-        //    }
-        //    else
-        //    {
-        //        highlight = null;
-        //    }
-        //}
-
-        //// Selection
-        //if (Input.GetMouseButtonDown(0))
-        //{
-        //    if (highlight)
-        //    {
-        //        if (selection != null)
-        //        {
-        //            selection.gameObject.GetComponent<Outline>().enabled = false;
-        //        }
-        //        selection = raycastHit.transform;
-        //        selection.gameObject.GetComponent<Outline>().enabled = true;
-        //        highlight = null;
-        //    }
-        //    else
-        //    {
-        //        if (selection)
-        //        {
-        //            selection.gameObject.GetComponent<Outline>().enabled = false;
-        //            selection = null;
-        //        }
-        //    }
-        //}
-
-        // Reset the highlight effect if an object was previously highlighted
+      
         if (highlight != null)
         {
             highlight.gameObject.GetComponent<Outline>().enabled = false;
             highlight = null;
         }
 
-        // Create a ray from the player's position, casting forward in the direction the player is facing
+       
         Ray ray = new Ray(player.position, player.forward);
 
-        // Check if the ray hits an object within a certain distance (e.g., 10 units)
         if (Physics.Raycast(ray, out raycastHit, interactDistance))
         {
-            // Assign the hit object to the highlight variable
             highlight = raycastHit.transform;
 
-            // Check if the object is tagged as "Selectable" and is not currently selected
             if (highlight.CompareTag("Selectable") && highlight != selection)
             {
-                // Enable or add the outline component if it doesn't exist
                 Outline outline = highlight.gameObject.GetComponent<Outline>();
                 if (outline != null)
                 {
@@ -104,7 +46,6 @@ public class OutlineSelection : MonoBehaviour
             }
             else
             {
-                // Reset highlight if the object is not selectable
                 highlight = null;
             }
         }
