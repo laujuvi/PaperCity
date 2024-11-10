a61418525f6bf86cbb349921531af3310b536963
laujuvi <laujuvi@hotmail.com>
2024-11-09 22:08:56 -0300

    Creo ShowOpenDoorMessage
    Creo el script para mostrar mensaje cuando se abre una puerta

---
+++ Assets/Scripts/IteractSystem/IInteracts/ShowOpenDoorMessage.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowOpenDoorMessage : MonoBehaviour
+{
+    [SerializeField] private PruebaInteractuable door;
+    [SerializeField] private BoxMessageManager boxMessageManager;
+    [SerializeField] private string doorMessage;
+    private bool isMessageBoxOpen = false;
+    void Start()
+    {
+        if (door != null)
+        {
+            door.OnDoorOpening += HandleDoorOpening;
+        }
+    }
+
+    private void HandleDoorOpening()
+    {
+        boxMessageManager.SendMessage("Detective", Color.white, doorMessage, Emotions.None);
+        door.OnDoorOpening -= HandleDoorOpening;
+    }
+}
