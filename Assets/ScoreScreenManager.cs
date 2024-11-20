30749e48f7c50e18fbefc9615b3c9eb16240b18a
laujuvi <laujuvi@hotmail.com>
2024-11-20 20:56:59 -0300

    Update ScoreScreenManager.cs
    Termino la logica del ScoreScreenManager

--- Assets/ScoreScreenManager.cs
+++ Assets/ScoreScreenManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,16 +11,41 @@ public class ScoreScreenManager : MonoBehaviour
 
     [Header("Imagenes")]
     [SerializeField] Texture scoreStarImage; 
-    [SerializeField] int totalStars;
-    private int totalClues;
+
+    [Header("Textos")]
+    [SerializeField] TextMeshProUGUI totalCluesText;
+    [SerializeField] TextMeshProUGUI resultMessageText;
+    [SerializeField] string[] resultMessages;
+
+    private float totalClues;
+    private float cluesObtained;
+    private int totalStars;
 
 
-    private void Start()
+    public void SetTotalClues (int total)
     {
-        UpdateScorePanel();
+        totalClues = total;
+    }
+    public void SetCluesObtained(int obtained)
+    {
+        cluesObtained = obtained;
+    }
+
+    private void SetTotalCluesText()
+    {
+        totalCluesText.SetText($"Encontraste {cluesObtained} pistas de {totalClues}.");
+    }
+
+    private void SetResultMessageText()
+    {
+        resultMessageText.SetText(resultMessages[totalStars-1]);
     }
     public void UpdateScorePanel()
     {
+        CalculateTotalScoreStarImage();
+        SetTotalCluesText();
+        SetResultMessageText();
+
         for (int i = 0; i < totalStars; i++)
         {
             GameObject newStarObj = new GameObject($"Star_{i + 1}");
@@ -28,5 +54,15 @@ public class ScoreScreenManager : MonoBehaviour
             RawImage newStar = newStarObj.AddComponent<RawImage>();
             newStar.texture = scoreStarImage;
         }
+    }
+    private void CalculateTotalScoreStarImage()
+    {
+        float percentage = (cluesObtained / totalClues) * 100f;
+
+        // Calcula las estrellas redondeando hacia abajo y asegurando un rango de 1 a 5
+        // Se divide por 20 porque queremos que el total de estrellas no supere las 5, entonces sabiendo que percentage<=100
+        // el resultado va a estar siempre dentro del rango de 1 a 5
+        totalStars = Mathf.Clamp(Mathf.CeilToInt(percentage / 20f), 1, 5);
+
     }
 }
